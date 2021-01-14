d999a114fff909b69a2001ce488f46c0d466436a
Alisa Gavrilova <64360518+GAD-Alisa@users.noreply.github.com>
2021-01-14 10:54:14 +0000

    Update GithubTest.cs

--- Assets/Scripts/GithubTest.cs
+++ Assets/Scripts/GithubTest.cs
@@ -7,7 +7,7 @@ public class GithubTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //a change
     }
 
     // Update is called once per frame
